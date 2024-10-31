3216232f66e9defe44a9350f11e0b4536e5891de
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2024-10-31 22:59:18 +0530

    Update title to 'Votely' and add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './styles/App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      {/* upper navbar */}
+      <div>
+        <div>
+          <div>
+            <a href="#">
+              <h1 className="logo">Votely</h1>
+            </a>
+          </div>
+          <div>
+            <ul className="langagues">
+              <li>
+                <a href="#">සිංහල</a>
+              </li>
+              <li>
+                <a href="#">தமிழ்</a>
+              </li>
+              <li>
+                <a href="#">English</a>
+              </li>
+            </ul>
+             </div>
+            
+        </div>
+      </div>
+      {/* lower navbar */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
