4526d5f0b3bea825df18053574a1f15108d2e7f4
Lasantha Pradeep <126849056+lasanthapradeep7@users.noreply.github.com>
2024-11-06 14:45:00 +0530

    Modify SignUp.js file and add new css

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -25,60 +25,51 @@ const SignUp = () => {
   return (
     <div className="main-container">
       <div className="welcome-section">
+
         <h3>Welcome to </h3>
         <h1>Votely</h1>
       </div>
       <div className="form">
         <h2>Sign Up</h2>
-        <p>Lorem ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <p>Welcome to Votely's Online Voting System, please register as a voter to vote in your preferred candidate.</p>
         <form onSubmit={handleSubmit}>
+
+        <label for="id_no">Voters ID No.</label>
           <input
             type="text"
             name="fullName"
-            placeholder="Enter full name"
+            placeholder="Voters ID No."
             value={form.fullName}
             onChange={handleChange}
             required
           />
+          <label for="password"> Create Password</label>
           <input
-            type="text"
-            name="vin"
-            placeholder="Enter VIN"
-            value={form.vin}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter Email address"
-            value={form.email}
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={form.password}
             onChange={handleChange}
             required
           />
           <input
             type="password"
-            name="password"
-            placeholder="Enter password"
-            value={form.password}
+            name="confirmpassword"
+            placeholder="Confirm password"
             onChange={handleChange}
             required
           />
-           <input
-            type="phonenumber"
-            name="phonenumber"
-            placeholder="Enter Phone number"
-            value={form.phonenumber}
-            onChange={handleChange}
-            required
-          />
-          <button type="submit">Sign Up</button>
+
+          <div className='terms'>
+            <input type="checkbox" name="terms" id="" />
+            <p>I agree to Votely's <a href="#">Terms & Conditions</a> & <a href="#">Privacy Policy</a></p>
+          </div>
+
+          <button type="submit">Register</button>
         </form>
+
         <p>
           Already have an account? <a href="/signin">Sign In</a>
-        </p>
-        <p>
-          By signing up you accept our <a href="/terms">terms & condition</a> & <a href="/privacy">privacy policy</a>
         </p>
       </div>
     </div>
