73a8ea3693f6be68cc83a989429234c7e51f8c8b
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2024-11-15 19:11:57 +0530

    Enhance Header component with logo, legal links, and contact information

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div>
-    
+    <div className="flex justify-evenly bg-indigo-600 text-white h-44">
+      <div className="justify-items-start content-center space-y-2 ">
+        <a href="#" className="">
+          <img src={Logo} alt="" className="w-40 " />
+        </a>
+        <div className="text-xs">Copyright © 2024. All rights reserved.</div>
+      </div>
+      <div className="justify-items-left p-8 content-center">
+        <div className="font-semibold ">Legal</div>
+        <div className="text-sm">
+          <div className="font-medium ">
+            <Link to="/terms-and-conditions">Terms and Conditions</Link>
+          </div>
+          <div className="font-medium">
+            <Link to="/privacy-policy">Privacy Policy</Link>
+          </div>
+          <div className="font-medium">
+            <Link to="/faqs">FAQs</Link>
+          </div>
+        </div>
+      </div>
+      <div className="justify-items-left p-8 content-center">
+        <div className="font-semibold">Contact Us</div>
+        <div className="text-sm">
+          <div className="font-medium">Address : Uva Wellassa University, Sri lanka</div>
+          <div className="font-medium">Email : Votely@gmail.com</div>
+          <div className="font-medium">Phone No : +94 076 169 3863</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
