66e71fe326d3b110fb1ccd194d3eaba724b285f6
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2024-11-15 23:57:07 +0530

    Refactor Header and Home components for improved layout and responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,33 +4,35 @@ import Logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className="flex justify-evenly bg-indigo-600 text-white h-44">
-      <div className="justify-items-start content-center space-y-2 ">
+    <div className="flex justify-evenly bg-indigo-600 text-white h-48">
+      <div className="justify-items-center content-center space-y-2 ">
         <a href="#" className="">
           <img src={Logo} alt="" className="w-40 " />
         </a>
         <div className="text-xs">Copyright © 2024. All rights reserved.</div>
       </div>
-      <div className="justify-items-left p-8 content-center">
-        <div className="font-semibold ">Legal</div>
+      <div className="justify-items-left p-8 content-center hidden md:block">
+        <div className="font-semibold pb-2">Legal</div>
         <div className="text-sm">
-          <div className="font-medium ">
+          <div className="font-medium pb-1">
             <Link to="/terms-and-conditions">Terms and Conditions</Link>
           </div>
-          <div className="font-medium">
+          <div className="font-medium pb-1">
             <Link to="/privacy-policy">Privacy Policy</Link>
           </div>
-          <div className="font-medium">
+          <div className="font-medium pb-1">
             <Link to="/faqs">FAQs</Link>
           </div>
         </div>
       </div>
-      <div className="justify-items-left p-8 content-center">
-        <div className="font-semibold">Contact Us</div>
-        <div className="text-sm">
-          <div className="font-medium">Address : Uva Wellassa University, Sri lanka</div>
-          <div className="font-medium">Email : Votely@gmail.com</div>
-          <div className="font-medium">Phone No : +94 076 169 3863</div>
+      <div className=" justify-items-left p-8 content-center hidden md:block ">
+        <div className="font-semibold pb-2">Contact Us</div>
+        <div className="text-sm ">
+          <div className="font-medium pb-1">
+            Address : Uva Wellassa University, Sri Lanka
+          </div>
+          <div className="font-medium pb-1">Email : Votely@gmail.com</div>
+          <div className="font-medium pb-1">Phone No : +94 076 169 3863</div>
         </div>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,58 @@
-import React from 'react'
-import Landing from '../assets/landing.svg'
+import React from "react";
+import Landing from "../assets/landing.svg";
 
 const Home = () => {
   return (
-    <div>
-      <div>Landing</div>
-      <div>Steps</div>
-      <div>Quote</div>
-      <div>Features</div>
+    <div className="">
+      <div className="flex flex-col lg:flex-row justify-around items-center gap-5 p-20 md:p-20 lg:px-48 lg:py-40">
+        <div className="text-center lg:text-left">
+          <div className="font-black text-3xl md:text-4xl lg:text-5xl">
+            Votely{" "}
+            <span className="font-bold text-2xl md:text-3xl lg:text-4xl">
+              Online Voting System
+            </span>
+          </div>
+          <div className="text-lg md:text-xl mt-4">
+            Let's make voting and elections easy for you.
+            This site ensures a secured voting session.
+          </div>
+          <button className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
+            Get Started
+          </button>
+        </div>
+        <div className="w-2/3 lg:w-1/2 mt-8 lg:mt-0  justify-items-center">
+          <img src={Landing} alt="Landing" className="lg:w-10/12 lg:h- md:w-1/2 md:h-1/2 sm:w-1/2 sm:h-1/2" />
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-col lg:flex-row justify-around items-center gap-5 p-5 md:p-10 lg:px-32 lg:pb-20 bg-slate-400">
+        <div className="text-center lg:text-left">
+          <div className="font-black text-3xl md:text-4xl lg:text-5xl">
+            Votely{" "}
+            <span className="font-bold text-2xl md:text-3xl lg:text-4xl">
+              Online Voting System
+            </span>
+          </div>
+          <div className="text-lg md:text-xl mt-4">
+            Let's make voting and elections easy for you.
+            This site ensures a secured voting session.
+          </div>
+          <button className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
+            Get Started
+          </button>
+        </div>
+        <div className="w-full lg:w-1/2 mt-8 lg:mt-0 hidden sm:block justify-items-center">
+          <img src={Landing} alt="Landing" className="lg:w-full lg:h- md:w-1/2 md:h-1/2 sm:w-1/2 sm:h-1/2" />
+        </div>
+      </div>
+      
+      <div className="text-center lg:text-left">
+        <div className="text-2xl font-semibold">Steps</div>
+        <div className="text-xl mt-4">Quote</div>
+        <div className="text-xl mt-4">Features</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
