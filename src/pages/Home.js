d4390caf075ae83c2cc863923a6cfcb3cdffbf9c
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2024-11-15 21:21:40 +0530

    Refactor Home page layout, integrate React Router, and enhance Navbar with responsive design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Landing from '../assets/landing.svg'
 
 const Home = () => {
   return (
     <div>
-      
+      <div>Landing</div>
+      <div>Steps</div>
+      <div>Quote</div>
+      <div>Features</div>
     </div>
   )
 }
